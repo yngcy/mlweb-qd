9a812cd439b8e0d593d6405a48ce31a2e62a9015
yngcy <2792783811@qq.com>
2024-04-05 22:44:54 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## 包说明
+
+- docs：存放项目相关文档
+- qd-backend：后端代码
+- qd-frontend: 前端代码
+- qd-devops：自动化环境部署
---
+++ qd-backend/src/test/java/cn/edu/swust/qd/qdservice/QdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.swust.qd.qdservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
